eaa4423be2f2afc6a6ee199956e4924eb6f981da
BarbaraButz <bbutz@uos.de>
2016-11-20 15:44:00 +0100

    enum Tokens edited, fn tokenize added

--- sheet4/task2/calculator.rs
+++ sheet4/task2/calculator.rs
@@ -1,16 +1,49 @@
 #[derive(Clone, Debug)]
 enum Token {
     Number(u32),
-    BracketExpression {
-        opening: char,
-        token: &'static Token,
-        closing: char,
-    },
-    ArithmeticExpression {
-        first_token: &'static Token,
-        oparator: char,
-        second_token: &'static Token,
-    },
+    Plus,
+    Minus,
+    OpeningBracket,
+    ClosingBracket,
+    //BracketExpression {
+    //    opening: char,
+    //    token: Box<Token>,
+    //    closing: char,
+    //},
+    //ArithmeticExpression {
+    //    first_token: Box<Token>,
+    //    oparator: char,
+    //    second_token: Box<Token>,
+    //},
+}
+
+impl Token {
+    fn tokenize(string: &mut String) -> Option<Vec<Token>> {
+        let mut tokens = Vec::new();
+        while !string.is_empty() {
+            let last_elem = string.pop();
+            match last_elem {
+                Some(' ') => {},
+                Some('(') => tokens.push(Token::OpeningBracket),
+                Some(')') => tokens.push(Token::ClosingBracket),
+                Some('+') => tokens.push(Token::Plus),
+                Some('-') => tokens.push(Token::Minus),
+                Some('0') => tokens.push(Token::Number(0)),
+                Some('1') => tokens.push(Token::Number(1)),
+                Some('2') => tokens.push(Token::Number(2)),
+                Some('3') => tokens.push(Token::Number(3)),
+                Some('4') => tokens.push(Token::Number(4)),
+                Some('5') => tokens.push(Token::Number(5)),
+                Some('6') => tokens.push(Token::Number(6)),
+                Some('7') => tokens.push(Token::Number(7)),
+                Some('8') => tokens.push(Token::Number(8)),
+                Some('9') => tokens.push(Token::Number(9)),
+                _ => return None,
+            }
+        }
+        tokens.reverse();
+        return Some(tokens);
+    }
 }
 
 fn main() {
