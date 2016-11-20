6942fbe5f6dc26708b51d33b739ca22cdbd98778
BarbaraButz <bbutz@uos.de>
2016-11-20 19:01:02 +0100

    all required functions of calculator.rs implemented, but not everything works

--- sheet4/task2/calculator.rs
+++ sheet4/task2/calculator.rs
@@ -1,8 +1,7 @@
 #[derive(Clone, Debug)]
 enum Token {
     Number(u32),
-    Plus,
-    Minus,
+    Operator(Op),
     OpeningBracket,
     ClosingBracket,
     //BracketExpression {
@@ -26,8 +25,8 @@ impl Token {
                 Some(' ') => {},
                 Some('(') => tokens.push(Token::OpeningBracket),
                 Some(')') => tokens.push(Token::ClosingBracket),
-                Some('+') => tokens.push(Token::Plus),
-                Some('-') => tokens.push(Token::Minus),
+                Some('+') => tokens.push(Token::Operator(Op::Plus)),
+                Some('-') => tokens.push(Token::Operator(Op::Minus)),
                 Some('0') => tokens.push(Token::Number(0)),
                 Some('1') => tokens.push(Token::Number(1)),
                 Some('2') => tokens.push(Token::Number(2)),
@@ -46,16 +45,152 @@ impl Token {
     }
 }
 
+#[derive(Clone, Debug)]
+enum Op {
+    Plus,
+    Minus,
+}
+
+impl Op {
+    fn apply(&self, number1: i32, number2: i32) -> i32 {
+        match *self {
+            Op::Plus => {
+                number1 + number2
+            },
+            Op::Minus => {
+                number1 - number2
+            },
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+enum Expr {
+    Leaf(u32),
+    Internal {
+        operator: Op,
+        children: Vec<Expr>,
+    },
+}
+
+impl Expr {
+    fn children_push(&mut self, expression: Expr) {
+        if let Expr::Internal{ref mut children, ..} = *self {
+            children.push(expression);
+        }
+    }
+    fn parse(tokens: &mut [Token]) -> Option<Expr> {
+        let length = tokens.len();
+        if length == 1 {
+            match tokens[0] {
+                Token::Number(x) => return Some(Expr::Leaf(x)),
+                _ => return None,
+            }
+        }
+        if let Token::OpeningBracket = tokens[0] {
+            if let Token::ClosingBracket = tokens[length-1] {
+                if let Some((_, elements)) = tokens.split_first_mut() {
+                    if let Some((_, newelements)) = elements.split_last_mut() {
+                        return Expr::parse(newelements);
+                    }
+                }
+            }
+        }
+        let mut bracketcounter = 0;
+        let mut expression;
+        for i in 0..length {
+            if bracketcounter == 0 {
+                match tokens[i] {
+                    Token::Operator(Op::Plus) => {
+                        expression = Expr::Internal {
+                            operator: Op::Plus,
+                            children: vec![],
+                        };
+                        let (mut child1, mut rest) = tokens.split_at_mut(i);
+                        if let Some((_, mut child2)) = rest.split_first_mut() {
+                            if let Some(expr1) = Expr::parse(child1) {
+                                if let Some(expr2) = Expr::parse(child2) {
+                                    expression.children_push(expr1);
+                                    expression.children_push(expr2);
+                                }
+                            }
+
+
+                        }
+                    },
+                    Token::Operator(Op::Minus) => {
+                        expression = Expr::Internal {
+                            operator: Op::Minus,
+                            children: vec![],
+                        };
+                        let (mut child1, mut rest) = tokens.split_at_mut(i);
+                        if let Some((_, mut child2)) = rest.split_first_mut() {
+                            if let Some(expr1) = Expr::parse(child1) {
+                                if let Some(expr2) = Expr::parse(child2) {
+                                    expression.children_push(expr1);
+                                    expression.children_push(expr2);
+                                }
+                            }
+                        }
+                    },
+                    Token::OpeningBracket => bracketcounter += 1,
+                    Token::ClosingBracket => bracketcounter -= 1,
+                    _ => {},
+                }
+            }
+
+        }
+
+        return None;
+    }
+
+    fn evaluate(&self) -> i32 {
+        match *self {
+            Expr::Leaf(x) => x as i32,
+            Expr::Internal {
+                ref operator, ref children
+            } =>
+            operator.apply(((children[0]).evaluate()), ((children[0]).evaluate()))
+        }
+    }
+}
+
+fn expr_test() -> bool {
+    let leaf1 = Expr::Leaf(3);
+    let leaf2 = Expr::Leaf(6);
+    let leaf3 = Expr::Leaf(1);
+    let internal1 = Expr::Internal{operator: Op::Minus, children: vec![leaf2, leaf3]};
+    let internal2 = Expr::Internal{operator: Op::Plus, children: vec![leaf1, internal1]};
+    internal2.evaluate() == 8
+}
+
 fn main() {
+    println!("{}", expr_test());
     loop {
         // Read input from the user and just do nothing when the input is empty
-        let input = read_string();
+        let mut input = read_string();
         if input.is_empty() {
             continue;
         }
 
-        // Debug output
-        println!("{}", input);
+        let tokenized = Token::tokenize(&mut input);
+        match tokenized {
+            Some(mut tokens) => {
+                // Debug output
+                println!("{}", input);
+                println!("{:#?}", tokens);
+                let parsetree = Expr::parse(&mut tokens);
+                if let Some(tree) = parsetree {
+                    let result = tree.evaluate();
+                    println!("Result: {}", result);
+                }
+            },
+            None => {
+                println!("Invalid Input!")
+            },
+        }
+
+
     }
 }
 
